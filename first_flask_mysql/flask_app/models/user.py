83d0b7ed9b09b6ade404dcccb80342f6c58363fd
Gavin Ewart <ggewart00@gmail.com>
2023-11-02 20:46:51 -0700

    created my first flask and mysql application

---
+++ first_flask_mysql/flask_app/models/user.py
@@ -0,0 +1,45 @@
+
+from flask_app import app
+from flask_app.config.mysqlconnection import connectToMySQL
+from flask import flash, session
+# import re
+# from flask_bcrypt import Bcrypt
+# bcrypt = Bcrypt(app)
+# The above is used when we do login registration, flask-bcrypt should already be in your env check the pipfile
+
+# Remember 'fat models, skinny controllers' more logic should go in here rather than in your controller. Your controller should be able to just call a function from the model for what it needs, ideally.
+
+class Friend: 
+    db = "first_flask_mysql"
+    def __init__( self , data ):
+        self.id = data['id']
+        self.first_name = data['first_name']
+        self.last_name = data['last_name']
+        self.occupation = data['occupation']
+        self.created_at = data['created_at']
+        self.updated_at = data['updated_at']
+    # Now we use class methods to query our database
+    @classmethod
+    def get_all(cls):
+        query = "SELECT * FROM friends;"
+        # make sure to call the connectToMySQL function with the schema you are targeting.
+        results = connectToMySQL(cls.db).query_db(query)
+        # Create an empty list to append our instances of friends
+        friends = []
+        # Iterate over the db results and create instances of friends with cls.
+        for friend in results:
+            friends.append(cls(friend))
+        return friends
+
+    # Create Users Models
+
+
+    # Read Users Models
+
+
+
+    # Update Users Models
+
+
+
+    # Delete Users Models
